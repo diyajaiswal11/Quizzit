23fba10841352419bf0fe60a3d9b10b33829e22f
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-06-11 01:23:50 +0530

    completed

--- quizzit/settings.py
+++ quizzit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +126,6 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+django_heroku.settings(locals())
 
