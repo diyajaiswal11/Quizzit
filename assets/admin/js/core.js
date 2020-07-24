a3551b221246d56de17080e7efa75a5f193225bf
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-07-24 23:36:41 +0530

    made changes

--- assets/admin/js/core.js
+++ assets/admin/js/core.js
@@ -30,7 +30,7 @@ function removeChildren(a) {
 
 // ----------------------------------------------------------------------------
 // Find-position functions by PPK
-// See https://www.quirksmode.org/js/findpos.html
+// See http://www.quirksmode.org/js/findpos.html
 // ----------------------------------------------------------------------------
 function findPosX(obj) {
     'use strict';
@@ -74,7 +74,13 @@ function findPosY(obj) {
 (function() {
     'use strict';
     Date.prototype.getTwelveHours = function() {
-        return this.getHours() % 12 || 12;
+        var hours = this.getHours();
+        if (hours === 0) {
+            return 12;
+        }
+        else {
+            return hours <= 12 ? hours : hours - 12;
+        }
     };
 
     Date.prototype.getTwoDigitMonth = function() {
@@ -99,6 +105,14 @@ function findPosY(obj) {
 
     Date.prototype.getTwoDigitSecond = function() {
         return (this.getSeconds() < 10) ? '0' + this.getSeconds() : this.getSeconds();
+    };
+
+    Date.prototype.getHourMinute = function() {
+        return this.getTwoDigitHour() + ':' + this.getTwoDigitMinute();
+    };
+
+    Date.prototype.getHourMinuteSecond = function() {
+        return this.getTwoDigitHour() + ':' + this.getTwoDigitMinute() + ':' + this.getTwoDigitSecond();
     };
 
     Date.prototype.getFullMonthName = function() {
@@ -139,9 +153,17 @@ function findPosY(obj) {
         return result;
     };
 
-    // ----------------------------------------------------------------------------
-    // String object extensions
-    // ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// String object extensions
+// ----------------------------------------------------------------------------
+    String.prototype.pad_left = function(pad_length, pad_string) {
+        var new_string = this;
+        for (var i = 0; new_string.length < pad_length; i++) {
+            new_string = pad_string + new_string;
+        }
+        return new_string;
+    };
+
     String.prototype.strptime = function(format) {
         var split_format = format.split(/[.\-/]/);
         var date = this.split(/[.\-/]/);
@@ -149,18 +171,18 @@ function findPosY(obj) {
         var day, month, year;
         while (i < split_format.length) {
             switch (split_format[i]) {
-            case "%d":
-                day = date[i];
-                break;
-            case "%m":
-                month = date[i] - 1;
-                break;
-            case "%Y":
-                year = date[i];
-                break;
-            case "%y":
-                year = date[i];
-                break;
+                case "%d":
+                    day = date[i];
+                    break;
+                case "%m":
+                    month = date[i] - 1;
+                    break;
+                case "%Y":
+                    year = date[i];
+                    break;
+                case "%y":
+                    year = date[i];
+                    break;
             }
             ++i;
         }
@@ -171,3 +193,20 @@ function findPosY(obj) {
     };
 
 })();
+// ----------------------------------------------------------------------------
+// Get the computed style for and element
+// ----------------------------------------------------------------------------
+function getStyle(oElm, strCssRule) {
+    'use strict';
+    var strValue = "";
+    if(document.defaultView && document.defaultView.getComputedStyle) {
+        strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
+    }
+    else if(oElm.currentStyle) {
+        strCssRule = strCssRule.replace(/\-(\w)/g, function(strMatch, p1) {
+            return p1.toUpperCase();
+        });
+        strValue = oElm.currentStyle[strCssRule];
+    }
+    return strValue;
+}
